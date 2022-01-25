6a1ac627b0a724bd30de22553dc9463b18a8a113
YesSeri <34012616+YesSeri@users.noreply.github.com>
2022-01-25 12:15:28 +0100

    fixed icons

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,10 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
+import GithubSVG from '../SVG/GithubSVG'
 import styles from './Footer.module.css'
 
 const Footer = () => {
 	return (
 		<div className={styles.container}>
-
+			<div className={styles.iconsContainer}>
+				<GithubSVG width='48' height='48'></GithubSVG>
+				<Image src='/mail.png' alt='mail-button' width={48} height={48}></Image>
+			</div>
+			<div className={styles.textContainer}>
+				<span className={styles.name}>HENRIK ZENKERT</span>
+			</div>
 		</div>
 	)
 }
